603a620c87e3285761470155c8da20907dd20286
Harsh5442 <146570956+Harsh5442@users.noreply.github.com>
2024-12-30 22:04:52 +0530

    Create eslint.config.js

---
+++ backend/eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Customize your ESLint rules here
+  },
+};
